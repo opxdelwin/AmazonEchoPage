3ca29f01c6bf9bfa6416b6a6ab36f7e4c9a5c700
Delwin Mathew <delu801@gmail.com>
2023-02-15 22:17:31 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
+// ignore_for_file: library_private_types_in_public_api
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    ),
+    const MaterialApp(debugShowCheckedModeBanner: false, home: Home()),
   );
 }
 
@@ -29,92 +27,93 @@ class _HomeState extends State<Home> {
     //
     return Container(
         height: 150,
-        color: Color(0xfffffeff),
+        color: const Color(0xfffffeff),
         child: Row(
           children: [
-            Container(
+            SizedBox(
                 height: 100,
                 width: 150,
-                child: Image.asset('$assetLoc', fit: BoxFit.cover)),
-            SizedBox(width: 5),
+                child: Image.asset(assetLoc, fit: BoxFit.cover)),
+            const SizedBox(width: 5),
             Expanded(
                 child: Container(
-                    color: Color(0xfffffeff),
+                    color: const Color(0xfffffeff),
                     height: 200,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('$title',
-                            style: TextStyle(
+                        Text(title,
+                            style: const TextStyle(
                                 fontWeight: FontWeight.w500,
                                 letterSpacing: 0.1,
                                 wordSpacing: -0.8,
                                 fontSize: 15)),
-                        SizedBox(height: 5),
+                        const SizedBox(height: 5),
                         Row(
                           children: [
-                            Container(
+                            SizedBox(
                               height: 15,
                               child: Image.asset('assets/rating.png',
                                   fit: BoxFit.fitHeight),
                             ),
-                            SizedBox(width: 7),
-                            Container(
+                            const SizedBox(width: 7),
+                            SizedBox(
                                 height: 15,
                                 child: Text(
-                                  '$rating',
-                                  style: TextStyle(color: Color(0xff6d6d6c)),
+                                  rating,
+                                  style:
+                                      const TextStyle(color: Color(0xff6d6d6c)),
                                 )),
-                            SizedBox(height: 10),
+                            const SizedBox(height: 10),
                           ],
                         ),
 
                         Row(
                           children: [
-                            Container(
+                            SizedBox(
                                 height: 25,
-                                child: Text('$currentPrice',
-                                    style: TextStyle(
+                                child: Text(currentPrice,
+                                    style: const TextStyle(
                                         fontWeight: FontWeight.w600,
                                         color: Color(0xffb33e2f),
                                         fontSize: 24))),
-                            SizedBox(
+                            const SizedBox(
                               width: 5,
                             ),
                             Container(
                                 height: 25,
                                 alignment: Alignment.bottomCenter,
-                                child: Text('$initPrice',
-                                    style: TextStyle(
+                                child: Text(initPrice,
+                                    style: const TextStyle(
                                         fontWeight: FontWeight.w700,
                                         color: Color(0xff6d6d6c),
                                         fontSize: 13,
                                         decoration:
                                             TextDecoration.lineThrough))),
-                            SizedBox(width: 5),
+                            const SizedBox(width: 5),
                             Container(
                                 height: 25,
                                 alignment: Alignment.bottomCenter,
                                 child: Text('Save $saveAmount',
-                                    style: TextStyle(
+                                    style: const TextStyle(
                                       fontWeight: FontWeight.w500,
                                       color: Color(0xff6d6d6c),
                                       fontSize: 14,
                                     ))),
-                            SizedBox(width: 5),
+                            const SizedBox(width: 5),
                             Container(
                                 height: 25,
                                 alignment: Alignment.bottomCenter,
                                 child: Text('($savePercent)',
-                                    style: TextStyle(
+                                    style: const TextStyle(
                                       fontWeight: FontWeight.w500,
                                       color: Color(0xff6d6d6c),
                                       fontSize: 14,
                                     )))
                           ],
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 5,
                         ),
 
@@ -122,13 +121,13 @@ class _HomeState extends State<Home> {
 
                         Row(
                           children: [
-                            Container(
+                            SizedBox(
                                 height: 15,
                                 width: 45,
                                 child: Image.asset('assets/prime.png')),
-                            SizedBox(width: 5),
-                            Text('FREE Delivery by '),
-                            Text(
+                            const SizedBox(width: 5),
+                            const Text('FREE Delivery by '),
+                            const Text(
                               'Saturday',
                               style: TextStyle(
                                   fontWeight: FontWeight.bold,
@@ -136,7 +135,7 @@ class _HomeState extends State<Home> {
                             )
                           ],
                         ),
-                        Padding(
+                        const Padding(
                           padding: EdgeInsets.fromLTRB(5, 2, 0, 0),
                           child: Text(
                             'September 5',
@@ -157,22 +156,23 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           toolbarHeight: 60,
           elevation: 0.0,
-          backgroundColor: Color(0xff85d9e3),
+          backgroundColor: const Color(0xff85d9e3),
           title: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Icon(Icons.arrow_back, color: Colors.black),
-              SizedBox(width: 10),
+              const Icon(Icons.arrow_back, color: Colors.black),
+              const SizedBox(width: 10),
               Expanded(
                   child: Container(
                       height: 35,
                       width: 900,
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(6)),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(6)),
                           border: Border.all(width: 0.5),
                           color: Colors.white),
                       child: Row(
-                        children: [
+                        children: const [
                           SizedBox(width: 5),
                           Icon(Icons.search_outlined,
                               color: Colors.black, size: 23),
@@ -191,8 +191,8 @@ class _HomeState extends State<Home> {
                                   color: Colors.black))
                         ],
                       ))),
-              SizedBox(width: 7),
-              Icon(Icons.mic_none_sharp, size: 25, color: Colors.black)
+              const SizedBox(width: 7),
+              const Icon(Icons.mic_none_sharp, size: 25, color: Colors.black)
             ],
           ),
         ),
@@ -202,11 +202,11 @@ class _HomeState extends State<Home> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                    constraints: BoxConstraints(
+                    constraints: const BoxConstraints(
                         minHeight: 35, minWidth: double.infinity),
-                    decoration: BoxDecoration(color: Color(0xffb2e9f1)),
+                    decoration: const BoxDecoration(color: Color(0xffb2e9f1)),
                     child: Row(
-                      children: [
+                      children: const [
                         SizedBox(width: 4),
                         Padding(
                             padding: EdgeInsets.all(5),
@@ -232,43 +232,43 @@ class _HomeState extends State<Home> {
                                 Border.all(width: 0.2, color: Colors.black54)),
                         child: Row(children: [
                           Padding(
-                            padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                            child: Container(
+                            padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                            child: SizedBox(
                                 height: 20,
                                 width: 55,
                                 child: Image.asset('assets/prime.png')),
                           ),
                           Padding(
-                            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
                             child: FlutterSwitch(
                               value: false,
                               onToggle: (value) {},
-                              inactiveToggleColor: Color(0xffffffff),
-                              inactiveColor: Color(0xffdddddd),
+                              inactiveToggleColor: const Color(0xffffffff),
+                              inactiveColor: const Color(0xffdddddd),
                               toggleSize: 15,
                               borderRadius: 10,
                               height: 25,
                               width: 45,
                             ),
                           ),
-                          SizedBox(width: 5)
+                          const SizedBox(width: 5)
                         ])),
                     Expanded(
                         child: Container(
-                            constraints: BoxConstraints(minHeight: 35),
+                            constraints: const BoxConstraints(minHeight: 35),
                             decoration: BoxDecoration(
                               border: Border.all(
-                                  width: 1.2, color: Color(0xffececec)),
+                                  width: 1.2, color: const Color(0xffececec)),
                             ),
-                            child: SizedBox(width: 20))),
+                            child: const SizedBox(width: 20))),
                     Container(
-                        constraints: BoxConstraints(minHeight: 35),
+                        constraints: const BoxConstraints(minHeight: 35),
                         decoration: BoxDecoration(
                             border: Border.all(
-                                width: 1.2, color: Color(0xffececec))),
-                        padding: EdgeInsets.fromLTRB(5, 0, 10, 0),
+                                width: 1.2, color: const Color(0xffececec))),
+                        padding: const EdgeInsets.fromLTRB(5, 0, 10, 0),
                         child: Row(
-                          children: [
+                          children: const [
                             Text('Filter',
                                 style: TextStyle(color: Color(0xff468e92))),
                             Icon(Icons.keyboard_arrow_down_sharp,
@@ -280,24 +280,24 @@ class _HomeState extends State<Home> {
 
                 // sponsered product
 
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 Container(
-                    color: Color(0xfffffeff),
+                    color: const Color(0xfffffeff),
                     child: Row(
                       children: [
-                        Container(
+                        SizedBox(
                             height: 75,
                             width: 150,
                             child: Image.asset('assets/echo_4.jpg',
                                 fit: BoxFit.cover)),
                         Expanded(
-                            child: Container(
+                            child: SizedBox(
                                 height: 75,
                                 // color: Colors.black
                                 child: Column(
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
-                                    children: [
+                                    children: const [
                                       Text(
                                         'Echo show 8 (2nd Gen) - 8" HD screen with Alexa and 13 MP Camera',
                                         style: TextStyle(
@@ -316,10 +316,10 @@ class _HomeState extends State<Home> {
 
                 Container(
                     width: double.infinity,
-                    color: Color(0xfffffeff),
-                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                    margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: Text('RESULTS',
+                    color: const Color(0xfffffeff),
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: const Text('RESULTS',
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 15))),
 
@@ -332,7 +332,9 @@ class _HomeState extends State<Home> {
                     '22%',
                     'assets/echo_1.jpg'),
                 Container(
-                    color: Color(0xfffffeff), child: Text(''), height: 10),
+                    color: const Color(0xfffffeff),
+                    height: 10,
+                    child: const Text('')),
 
                 getResult(
                     'Echo Dot (4rd Gen) - #1 smart speaker brand in India with Alexa (White)',
@@ -343,7 +345,9 @@ class _HomeState extends State<Home> {
                     '0%',
                     'assets/echo_2.jpg'),
                 Container(
-                    color: Color(0xfffffeff), child: Text(''), height: 10),
+                    color: const Color(0xfffffeff),
+                    height: 10,
+                    child: const Text('')),
 
                 getResult(
                     'Echo (4rd Gen, 2020 release) | Premimum sound powered by Dolby and Alexa (Black)',
@@ -354,11 +358,16 @@ class _HomeState extends State<Home> {
                     '59%',
                     'assets/echo_3.jpg'),
                 Container(
-                    color: Color(0xfffffeff), child: Text(''), height: 10),
+                    color: const Color(0xfffffeff),
+                    height: 10,
+                    child: const Text('')),
 
                 getResult('Echo (3rd Gen) with Alexa (Grey)', '2,150', '3,499',
                     '4,499', '1000', '22%', 'assets/echo_1.jpg'),
-                Container(color: Color(0xfffffeff), child: Text(''), height: 10)
+                Container(
+                    color: const Color(0xfffffeff),
+                    height: 10,
+                    child: const Text(''))
               ],
             )
           ],
